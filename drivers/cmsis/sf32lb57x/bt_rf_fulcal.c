ca091f7df5970032930ae796298b37c80f376c68
yeliao <yeliao@sifli.com>
2026-05-29 14:29:38 +0800

    [bug][bt] Added empty bt_rf_cal() in 57x to avoid build error
    redmine: #12444, REDMINE-12444
    
    ext-redmine: #id, REDMINE-id
    
    [Description in detail]
    Describe the change in more detail, e.g. how is the problem fixed, why do you make the modification in this way, explain the feature in more details
    
    Change-Id: I5597ab7c0fbbe33f63c526458bed8cdaf53c2600

---
+++ drivers/cmsis/sf32lb57x/bt_rf_fulcal.c
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2025 SiFli Technologies(Nanjing) Co., Ltd
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "rtconfig.h"
+
+void bt_rf_cal_index(void)
+{
+
+}
+
